7fe8af73da986d1609bc5b4889cf66d81cd8543e
Stas Pozdeev <stas.pazdzeev@gmail.com>
2017-06-09 20:06:55 +0300

    Create function get count tracks Music

---
+++ Music/Music/CountTracksMusic.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [dbo].[CountTracksMusic]
+(
+	@MusicId INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @CountTracksMusic INT
+	SELECT @CountTracksMusic = COUNT(*) FROM dbo.[Track] WHERE MusicId = @MusicId
+	RETURN @CountTracksMusic
+END
