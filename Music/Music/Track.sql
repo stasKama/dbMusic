50d8ed7795d91ef78fb5acfb3269a1a443b30502
Stas Pozdeev <stas.pazdzeev@gmail.com>
2017-06-09 20:03:14 +0300

    Create trigger after and before

--- Music/Music/Track.sql
+++ Music/Music/Track.sql
@@ -7,3 +7,58 @@
 	FOREIGN KEY ([MusicId]) REFERENCES [Music] ([Id]) ON DELETE CASCADE,
 	FOREIGN KEY ([MixId]) REFERENCES [Mix] ([Id]) ON DELETE CASCADE
 )
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_Mix_For]
+    ON [dbo].[Track]
+    FOR INSERT
+    AS
+    BEGIN
+        SET NoCount ON
+		DECLARE @MusicId INT, @CountMusic INT, @CountLicenses INT
+		IF EXISTS (SELECT * FROM inserted)
+		BEGIN
+			 SELECT @MusicId = inserted.MusicId FROM inserted
+			 SELECT @CountMusic = TrackCount, @CountLicenses = TrackLicenses FROM dbo.[Music] WHERE Id = @MusicId
+			 IF @CountMusic > @CountLicenses
+			 BEGIN
+				ROLLBACK TRAN
+				RETURN
+			 END
+		END
+    END
+GO
+
+CREATE TRIGGER [dbo].[Trigger_Mix_After]
+   ON [dbo].[Track]
+    AFTER DELETE, INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+		DECLARE @MusicId INT, @MixId INT, @CountMusic INT, @CountLicenses INT, @PrimaryGenre VARCHAR(40), @SecondaryGenre VARCHAR(40)
+		IF UPDATE(MusicId) 
+		BEGIN
+			 SELECT @MusicId = inserted.MusicId FROM inserted
+			 SELECT @CountMusic = TrackCount, @CountLicenses = TrackLicenses FROM dbo.[Music] WHERE Id = @MusicId
+			 SELECT @CountMusic
+			 IF ((SELECT COUNT(*) FROM inserted WHERE MusicId = @MusicId) + @CountMusic) > @CountLicenses
+			 BEGIN
+				ROLLBACK TRAN
+				RETURN
+			 END
+		END
+		IF EXISTS (SELECT * FROM inserted)
+		BEGIN
+			SELECT @MusicId = MusicId, @MixId = MixId FROM inserted
+			SELECT @PrimaryGenre = PrimaryGenre, @SecondaryGenre = SecondaryGenre FROM [dbo].[GetGenre](@MixId)
+		END
+		ELSE
+		BEGIN
+			SELECT @MusicId = MusicId, @MixId = MixId FROM deleted
+			SELECT @PrimaryGenre = PrimaryGenre, @SecondaryGenre = SecondaryGenre FROM [dbo].[GetGenre](@MixId)
+		END
+		UPDATE dbo.[Music] SET dbo.[Music].TrackCount = [dbo].[CountTracksMusic](@MusicId) WHERE Id = @MusicId
+		UPDATE dbo.[Mix] SET dbo.[Mix].PrimaryGenre = @PrimaryGenre,  dbo.[Mix].SecondaryGenre = @SecondaryGenre WHERE Id = @MixId
+    END
+GO
