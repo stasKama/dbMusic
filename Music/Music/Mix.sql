7eeb12d635fc3319019c66b85e2e40affb0b2e30
Stas Pozdeev <stas.pazdzeev@gmail.com>
2017-06-09 22:14:57 +0300

    Create trigger сounting the number of created mixtures

--- Music/Music/Mix.sql
+++ Music/Music/Mix.sql
@@ -7,3 +7,29 @@
 	[SecondaryGenre] VARCHAR(40) NULL,
     FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
 )
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_Get_Count_New_Mix]
+    ON [dbo].[Mix]
+    AFTER INSERT
+    AS
+    BEGIN
+        SET NoCount ON
+		DECLARE @UserId INT
+		
+		DECLARE mixCursor CURSOR LOCAL STATIC FOR
+			SELECT inserted.UserId FROM inserted
+
+		OPEN mixCursor
+		FETCH FIRST FROM mixCursor INTO @UserId
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			UPDATE dbo.[User] SET CountCreateMix = CountCreateMix + 1 WHERE Id = @UserId
+			FETCH NEXT FROM mixCursor INTO @UserId
+		END
+		CLOSE mixCursor
+		DEALLOCATE mixCursor
+    END
+
+GO
