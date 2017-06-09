886cd64c4b9c9bcc349d3df8eb8c2b8b0114fde6
Stas Pozdeev <stas.pazdzeev@gmail.com>
2017-06-09 13:16:30 +0300

    Add field Genre in dbo.Music table and Cascade delete in dbo.Track table

--- Music/Music/Track.sql
+++ Music/Music/Track.sql
@@ -4,6 +4,6 @@
 	[MusicId] INT,
 	[MixId] INT NOT NULL,
 	UNIQUE([MusicId], [MixId]),
-	FOREIGN KEY ([MusicId]) REFERENCES [Music] ([Id]),
-	FOREIGN KEY ([MixId]) REFERENCES [Mix] ([Id])
+	FOREIGN KEY ([MusicId]) REFERENCES [Music] ([Id]) ON DELETE CASCADE,
+	FOREIGN KEY ([MixId]) REFERENCES [Mix] ([Id]) ON DELETE CASCADE
 )
